48c40581df5884f275e45cc2d3c2c1f11333f758
lynsiearagon <lynsie.aragon@gmail.com>
2022-12-07 16:41:06 -0500

    User initialize established

--- questions.rb
+++ questions.rb
@@ -6,5 +6,75 @@ class QuestionsDatabase < SQLite3::Database
 
     def initialize
         super('questions.db')
+        self.type_translation = true
+        self.results_as_hash = true 
     end
 end
+
+
+class User
+
+    attr_accessor :fname, :lname
+
+    def self.all
+        data = QuestionsDatabase.instance.execute("SELECT * FROM users")
+        data.map { |datum| User.new(datum) }
+    end
+
+    def self.find_my_id()
+
+    end
+
+    def self.find_my_name(fname, lname)
+        # name = QuestionsDatabase.instance.execute(<<-SQL, )
+    end
+
+    def initialize(options)
+        @id = options['id']
+        @fname = options['fname']
+        @lname = options['lname']
+    end
+
+    
+
+end
+
+
+
+class Question
+
+    def self.find_my_id
+        
+    end
+
+end
+
+
+class QuestionFollow
+
+    def self.find_my_id
+        
+    end
+
+end
+
+
+
+class Reply
+
+    def self.find_my_id
+        
+    end
+
+end
+
+
+class QuestionLike
+
+    def self.find_my_id
+        
+    end
+
+end
+
+
